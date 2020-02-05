dae3e5d3655a7e19e8af5c3c277e44d10b2b02bf
Michel Lima <michelpl@yahoo.com.br>
2020-02-04 21:24:28 -0300

    :tada: Create laravel application.

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('/V1')->middleware('cors')->group(function () {
+
+    Route::get('/', function(){
+        return;
+    });
+
+    Route::middleware('cors')->get('/teste', function(){
+        return;
+    });
+
+    Route::post('register', 'API\UserController@register');
+    Route::post('login', 'API\UserController@login');
+
+    /*Route::group(['middleware' => 'auth:api'], function() {
+        Route::post('details', 'API\UserController@details');
+    });*/
+});
